c5795cb124c4adf4cadf994ba1fb204a11cc9b7e
Braxton Wood <brakwood@gmail.com>
2023-01-05 20:34:14 -0700

    successful web response

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!!!!!!!!!!!!!!')
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+const PORT = 1337;
+app.listen(PORT, () =>{
+    console.log(`app listening to port ${PORT}`)
+});
